5212e15a8608ebdaba074aed68ada3f40b0b38db
ShubhamNstark <321Shubh@m>
2020-11-19 21:43:39 +0530

    Database Added

---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+     <head> 
+	<center><h1>Login Page</h1></center>
+     <title> Login Page </title> 
+      </head>
+          <body>
+	<form action="login.php" method="post">   
+	<center>
+	<table>   
+	<tr><td>Email Id:</td><td> <input type="email" name="email"/></td></tr>
+	<tr><td>Password:</td><td> <input type="password" name="password" minlength="8"/></td></tr>
+	<tr rowspan="2"><td colspan="2" align="right"><input type="submit" value="Login Up"/></td></tr>  
+	</table>
+	</center>  
+	</form>
+          </body>
+</html>   
--- login.php
+++ login.php
@@ -1,8 +1,24 @@
 <?php  
+$host = 'localhost:3306';  
+$user = 'root';  
+$pass = '';  
+$dbname = 'php_application';
+$conn = mysqli_connect($host, $user, $pass,$dbname);  
+if(! $conn )  
+{  
+  die('Could not connect: ' . mysqli_error());  
+}  
+##echo 'Connected successfully';      
+  $sql = 'SELECT EMAIL, PASSWORD FROM login';
+   $retval = mysqli_query($conn, $sql);
+   $row=mysqli_fetch_assoc($retval);
+   $db_email=$row["EMAIL"];
+   $db_password=$row["PASSWORD"];
+echo "{$row["EMAIL"]}";
 $email=$_POST["email"];//receiving name field value in $name variable  
 $password=$_POST["password"];//receiving password field value in $password variable  
-if($email =="shubham@gmail.com"){
-	if($password=="321sakshi")
+if($email ==$db_email){
+	if($password==$db_password)
 	{
 		echo "Success, entered password and email id is correct";
 	}
@@ -12,5 +28,6 @@ if($email =="shubham@gmail.com"){
 }
 else{
 echo "Email Id Incorrect";
-}  
+}
+mysqli_close($conn);  
 ?>    

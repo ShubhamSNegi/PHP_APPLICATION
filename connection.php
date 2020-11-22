7e216d6fde18d35974632d812d7d1f3315f39ca6
ShubhamSNegi <shubhamsinghnegi45@gmail.com>
2020-11-22 11:10:12 +0530

    formatting corrected

--- connection.php
+++ connection.php
@@ -1,10 +1,10 @@
 <?php 
-$host = 'localhost:3306';  
-$user = 'root';  
-$pass = '';  
-$dbname = 'php_application';
-$conn = mysqli_connect($host, $user, $pass,$dbname);
-if(! $conn )  
+$db_hostname = 'localhost:3306';  
+$db_username = 'root';  
+$db_password = '';  
+$db_name = 'php_application';
+$db_connection = mysqli_connect($db_hostname, $db_username, $db_password, $db_name);
+if( ! $db_connection )  
    {  	
        die('Could not connect: ' . mysqli_error());  
    }
--- login.php
+++ login.php
@@ -1,29 +1,32 @@
 <?php  
-   require_once ("connection.php");        
-   	if(isset($_POST["email"]) && isset($_POST["password"]))
+   require_once  "connection.php" ;        
+   	if( isset($_POST["email"])  &&  isset( $_POST["password"] ) )
 	{
-		$email=mysqli_real_escape_string($conn, $_POST["email"]);  
-   		$password=mysqli_real_escape_string($conn,  $_POST["password"]);
-		$password = password_hash($password, PASSWORD_DEFAULT);
-		$sql = "SELECT  EMAIL, PASSWORD  FROM  login WHERE  EMAIL = '$email' ";
-		$result = mysqli_query($conn, $sql);
-		if(mysqli_num_rows($result) > 0){
-		while($row = mysqli_fetch_assoc($result)) {
-			 if(password_verify( $row["PASSWORD"],$password))  
+		$email = mysqli_real_escape_string( $db_connection, $_POST["email"] );  
+   		$password = mysqli_real_escape_string( $db_connection,  $_POST["password"] );
+		$password = password_hash( $password, PASSWORD_DEFAULT );
+		$sql_query = "SELECT  EMAIL, PASSWORD  FROM  login WHERE  EMAIL = '$email' ";
+		$result = mysqli_query($db_connection, $sql_query);
+		if( mysqli_num_rows($result) > 0 )
+		{
+		while( $row = mysqli_fetch_assoc($result )) 
+		{
+			 if( password_verify( $row["PASSWORD"], $password ))  
                      		{  
-                          			echo "SUCESSFULLY LOGED IN ";
+                          			echo  "SUCESSFULLY LOGED IN ";
                      		} 
 			else{
-				echo "Incorrect Password";
+				echo  "Incorrect Password";
 			}
 		}
 		}
               		else{
-                     		echo "Incorrect Email Id";
-                   		}        		
+                     		echo  "Incorrect Email Id";
+                   	}        		
 	}
-	else{
-		echo "No Data Entered";
+	else
+	{
+		echo "Username or Password not entered";
 	}
-	   mysqli_close($conn);  
+	   mysqli_close($db_connection);  
 ?>    
